02e73ade763dbed2af6747a79f2f04d812ac6b8a
Marc Magon <marc.magon@bayport.co.za>
2020-03-05 10:38:36 +0200

    first commit

---
+++ project1/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module jdk.jlink.bug {
+    requires org.apache.poi.ooxml;
+    requires com.guicedee.guicedservlets.swagger;
+    requires com.guicedee.services.swaggerui;
+    requires org.apache.cxf;
+
+    requires org.hibernate.orm.core;
+    requires java.transaction;
+
+    requires com.hazelcast.all;
+    requires undertow.servlet;
+
+}
