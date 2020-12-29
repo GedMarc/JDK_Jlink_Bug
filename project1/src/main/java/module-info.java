d45a1a7d5272e6e4a17ae0f98a0d1a1944e2d9ac
GedMarc <ged_marc@hotmail.com>
2020-12-29 17:14:27 +0200

    Reproduces method too large on jdk 15

--- project1/src/main/java/module-info.java
+++ project1/src/main/java/module-info.java
@@ -1,13 +1,19 @@
 module jdk.jlink.bug {
-    requires org.apache.poi.ooxml;
-    requires com.guicedee.guicedservlets.openapi;
-    requires com.guicedee.services.swaggerui;
-    requires org.apache.cxf;
-
-    requires org.hibernate.orm.core;
-    requires java.transaction;
-
-    requires com.hazelcast.all;
-    requires undertow.servlet;
-
+	requires org.apache.poi.ooxml;
+	requires com.guicedee.guicedservlets.openapi;
+	requires com.guicedee.services.swaggerui;
+	requires org.apache.cxf;
+	
+	requires org.hibernate.orm.core;
+	requires java.transaction;
+	
+	requires jakarta.faces;
+	requires java.desktop;
+	requires com.github.librepdf.openpdf;
+	requires com.jwebmp.plugins.bootstrap4;
+	requires com.jwebmp.plugins.bs4.quickforms;
+	
+	requires com.hazelcast.all;
+	requires undertow.servlet;
+	
 }
